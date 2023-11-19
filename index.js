3a4d1af93efcf00d44655dd4d4b3356607226e57
Kobzar Ivan <kobzar.ivan1@gmail.com>
2023-11-19 21:54:53 +0400

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+import { webkit } from 'playwright';
+
+const browser = await webkit.launch();
+const newsPage = await browser.newPage();
+
+await newsPage.goto('https://www.khaleejtimes.com/asia/uae/dubai');
+
+await newsPage.screenshot({ path: `screenshot-${new Date().toISOString()}.png`, fullPage: true });
